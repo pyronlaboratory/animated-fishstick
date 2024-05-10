26c73e8aacf01efbf45e48b9305216ea2afc847d
Ronnie <ronnie94official@gmail.com>
2024-05-10 10:31:25 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,11 +1,4 @@
-/**
- * @description Returns `false`.
- * 
- * @returns { false } `false`.
- * 
- * 		- The return value is `false`, indicating that the function does not return any
- * value.
- */
+
 const fn = () => {
    return false;
 
